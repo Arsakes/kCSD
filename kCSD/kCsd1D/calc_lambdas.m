098ceb290e27e40213bff6603451ad26509e9a13
Peter <elminister@interia.pl>
2014-09-27 20:31:45 +0200

    Deleted old kCSD by J.Potworowski.

--- kCSD/kCsd1D/calc_lambdas.m
+++
@@ -1,9 +0,0 @@
-%
-% External method for calculating set over we perform cross-validation
-% Lambdas are dense around zero
-%
-function lambdas = calc_lambdas( maxLambda )
-    x = 0:0.02:10;
-    lambdas = maxLambda./(2.^x); %dense around zero
-    lambdas = [lambdas, 0];
-end
--- kCSD/kCsd1D_ICA/ICA_preprocessing.m
+++
@@ -1,9 +0,0 @@
-function ICA_data = ICA_preprocessing(input)
-
-[nx, ~] = size(input);
-mixtures = stripmean(input,'st');
-fprintf('\nPerforming Singular Value Decomposition (SVD), wait...\n\n');
-[U D V] = svd(mixtures, 0);
-
-ICA_data = struct('mixtures', mixtures, 'nx', nx,...
-        'U', U, 'V', V,  'D', D);
