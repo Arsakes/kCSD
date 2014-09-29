a9745cf28c5f5acfb0785b558df502da128e77e5
Peter <elminister@interia.pl>
2014-09-29 20:16:56 +0200

    Dodano film potwierdzający skuteczność metody w 3d.

---
+++ pietrko_kCSD/benchmark/playWithRat.m
@@ -0,0 +1,17 @@
+%
+% Script to benchmark the kCSD programs on real data 
+% data sets come from some rat experiment
+%
+% MATLAB ONLY 
+%  
+% by Piotr Stępnicki (2014)
+
+% Remember the current location
+%% plays the rat movie
+%
+load('brainRatSuperstar.mat');
+[h,w,p]=size(F(1).cdata);
+hf=figure;
+set(hf, 'position', [150,150,w,h]);
+axis off
+movie(hf,F);
--- pietrko_kCSD/benchmark/rat3D.m
+++ pietrko_kCSD/benchmark/rat3D.m
@@ -22,52 +22,52 @@ load('rat3D.mat');
  
 
 % DEFINITION OF NEEDED GRIDS
-x=linspace(0,2.1,4);
-y=linspace(0,2.8,5);
-z=linspace(0,4.2,7);
+y=linspace(0,4,4);
+x=linspace(0,5,5);
+z=linspace(0,7,7);
 n_el = 4*5*7;
 [X,Y,Z]=meshgrid(x,y,z);
 
 src_grid=[reshape(X,n_el,1), reshape(Y,n_el,1), reshape(Z,n_el,1)];
 
 % test for grid
-plot3(src_grid(:,1), src_grid(:,2), src_grid(:,3),'.');
+%plot3(src_grid(:,1), src_grid(:,2), src_grid(:,3),'.');
 % src grid
 
 % out_grid
-x=linspace(0,2.1,12);
-y=linspace(0,2.8,15);
-z=linspace(0,4.2,21);
+y=linspace(0,4,12);
+x=linspace(0,5,15);
+z=linspace(0,7,21);
 n_o = 12*15*21;
 [Xo,Yo,Zo]=meshgrid(x,y,z);
 out_grid=[reshape(Xo,n_o,1), reshape(Yo,n_o,1), reshape(Zo,n_o,1)];
 
 
 % src_grid
-x=linspace(0,2.1,5);
-y=linspace(0,2.8,6);
-z=linspace(0,4.2,8);
+y=linspace(0,4,5);
+x=linspace(0,5,6);
+z=linspace(0,7,8);
 n_b = 5*6*8;
 [Xb,Yb,Zb]=meshgrid(x,y,z);
 base_grid=[reshape(Xb,n_b,1), reshape(Yb,n_b,1), reshape(Zb,n_b,1)];
 
-size(X)
-size(src_grid)
 % getting data in reasonable format
-V=zeros(14000,n_el);
+V=zeros(n_el,14000);
 for i=1:14000
-  V(i,:)=reshape(field_potentials(i,:,:),n_el,1,1);
+  V(:,i)=reshape(field_potentials(i,:,:,:),n_el,1);
 end
+% columns - constat time
 
 
 % stupid part TODO fixe kcsd to be able to receive 
 base_grid=base_grid';
 out_grid=out_grid';
 src_grid=src_grid';
-V=V';
+%size(src_grid)
+%size(V)
 
 % gauss radius
-sigma = 1/5;
+sigma = 0.9;
 g = kcsd(src_grid, out_grid, base_grid,V ,sigma);
 g = estimate(g);
 
@@ -81,26 +81,39 @@ g = estimate(g);
 %disp(pietrkoTime/jasioTime);
 
 % PLOTTING
-% ciekawe czy to działas
-CSD = reshape(g.CSD',14000, 12,15,21);
+
+CSD = reshape(g.CSD,12,15,21, 14000);
 % setting maximal values
+cma=max(max(max(max(CSD))));
+cmi=min(min(min(min(CSD))));
 
 
 % choosing time moment
-CSD_frame = squeeze(CSD(500,:,:,:));
-cma=max(max(max(CSD_frame)));
-cmi=min(min(min(CSD_frame)));
+time = 5;
+frame=@(ts) int32(14000*(ts+10)/30);
+
 
 %size(CSD_frame)
 %size(Xo)
 %size(Yo)
 %size(Zo)
+j=1;
 % just seven slices 
-for i=1:7
-  subplot(3,3,i)
-  z=(i-1)*4.2/6;
-  ind = i*2-1;
-  pcolor(Xo(:,:,1)', Yo(:,:,1)', CSD_frame(:,:,ind) ),shading('interp');
-  caxis([cmi,cma]);
+Z=peaks;
+fig=figure('Renderer', 'zbuffer', 'position', [100,100, 800,600]);
+set(gca,'NextPlot','replaceChildren');
+surf(Z);
+for t=linspace(-9,19,1024)
+  CSD_frame = squeeze(CSD(:,:,:,frame(t)));
+  for i=1:7
+    subplot(3,3,i)
+    ind = i*3-1;
+    pcolor(Xo(:,:,1), Yo(:,:,1), CSD_frame(:,:,ind) ),shading('interp');
+    title(strcat('t=',num2str(t)));
+    caxis([cmi,cma]);
+  end
+  F(j)=getframe(fig);
+  j=j+1;
 end
-
+close all;
+save brainRatSuperstar.mat -v7 F;
